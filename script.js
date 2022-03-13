4fd8ba71949bea052eaf25ee8f08bc824223d0f1
evariste1963 <evariste@hotmail.co.uk>
2022-03-13 16:53:33 +0000

    colors eventlistner tidy up

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const h3 = document.querySelector("h3");
+const body = document.querySelector("#gradient");
 const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
-const body = document.querySelector("#gradient");
+const h3 = document.querySelector("h3");
 
 h3.textContent = `linear-gradient(to right, ${color1.value}, ${color2.value}) ;`;
 
@@ -12,5 +12,6 @@ const setGradient = function () {
   h3.textContent = body.style.background + ";";
 };
 
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+[color1, color2].forEach(function (color) {
+  color.addEventListener("input", setGradient);
+});
