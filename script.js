69fc63b73d84a3ef8c1c0805c9d8ca90278eec17
evariste1963 <evariste@hotmail.co.uk>
2022-03-13 16:11:53 +0000

    1st commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const h3 = document.querySelector("h3");
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+const body = document.querySelector("#gradient");
+
+const setGradient = function () {
+  body.style.background = `Linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  h3.textContent = body.style.background + ";";
+};
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
