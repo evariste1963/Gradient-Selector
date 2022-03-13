443e805bf42275a4d04d54938d99fc12154913f6
evariste1963 <evariste@hotmail.co.uk>
2022-03-13 16:36:50 +0000

    answers 1 & 2

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
 const body = document.querySelector("#gradient");
 
+h3.textContent = `linear-gradient(to right, ${color1.value}, ${color2.value}) ;`;
+
 const setGradient = function () {
-  body.style.background = `Linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
   h3.textContent = body.style.background + ";";
 };
 
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
