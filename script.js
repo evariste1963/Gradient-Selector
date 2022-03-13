237ad66fa256b81451f23fe5e89845ec866c5513
evariste1963 <evariste@hotmail.co.uk>
2022-03-13 19:24:19 +0000

    refactoring

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
 const h3 = document.querySelector("h3");
 
-h3.textContent = `linear-gradient(to right, ${color1.value}, ${color2.value}) ;`;
+//run setGradient function on startup/refresh
+setGradient();
 
-const setGradient = function () {
+function setGradient() {
   body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
   h3.textContent = body.style.background + ";";
-};
+}
 
 [color1, color2].forEach(function (color) {
   color.addEventListener("input", setGradient);
