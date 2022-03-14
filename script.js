685496542131537875ccb655fa0962ec15d24b17
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-03-14 12:39:50 +0000

    includes a direction selector

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const body = document.querySelector("#gradient");
 let color1 = document.querySelector("#color1");
 let color2 = document.querySelector("#color2");
 const h3 = document.querySelector("h3");
+const direction = document.querySelector("#direction");
 const randomBtn = document.querySelector("#randomBtn");
 
 //run setGradient function on startup/refresh
@@ -11,13 +12,14 @@ renderGradient();
 
 //create color string
 function generateColorString() {
-  return `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  return `linear-gradient(to ${direction.value}, ${color1.value}, ${color2.value})`;
 }
 
 // render gradient
 function renderGradient() {
   body.style.background = generateColorString();
   randomBtn.style.background = generateColorString();
+  direction.style.background = generateColorString();
   h3.textContent = `${body.style.background};`;
 }
 
@@ -49,3 +51,5 @@ function generateColor() {
 
 //random bakground button
 randomBtn.addEventListener("click", setColor);
+
+direction.addEventListener("change", renderGradient);
